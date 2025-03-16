ef8ff0f3c2bf558e5b0a2b5434246347459b89ec
John Still <john.m.still@vumc.org>
2025-03-16 13:56:29 -0500

    Port online norm algo from old tools repo; add a version for torch modules

---
+++ src/cml_tools/neural_net/online_norm.py
@@ -0,0 +1,91 @@
+"""A Pytorch Module for calculating model mean and variance inputs online"""
+# see also: ../online_norm.py, which is designed for aggregating across pandas
+# dataframes with assumed heterogeneous data channels. The basic update
+# algorithm is the same though.
+
+# TODO: fill_nan currently accepts None or a float; but we could use strings
+# like "p01" (or something) to draw from the learned Gaussian and use that as
+# the fill instead.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class OnlineStandardScaler(nn.Module):
+    """Shifts input instances by online estimations of mean and variance"""
+    def __init__(self, num_features, eps=1e-5, frozen=False, fill_nan=None,
+                 device=None, dtype=None):
+        super().__init__()
+        self.eps = eps
+        self.frozen = frozen
+        self.fill_nan = fill_nan
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        running_mean = torch.zeros(num_features, **factory_kwargs)
+        running_var = torch.ones(num_features, **factory_kwargs)
+        # If we are nan-safe we may need counts per individual channel
+        if self.fill_nan is None:
+            running_num = torch.tensor(0, **factory_kwargs)
+        else:
+            running_num = torch.zeros(num_features, **factory_kwargs)
+        self.register_buffer('running_mean', running_mean)
+        self.register_buffer('running_var', running_var)
+        self.register_buffer('running_num', running_num.to(torch.long))
+
+    def reset_running_stats(self):
+        self.running_mean.zero_(0)
+        self.running_var.copy_(1)
+        self.running_num.zero_(0)
+
+    def update_running_stats(self, batch):
+        with torch.no_grad():
+            # get the previous running count, mean, var
+            pn = self.running_num.clone()
+            pv = self.running_var.clone()
+            pm = self.running_mean.clone()
+            # There is no torch.nanvar yet; we have to calculate ourselves:
+            # Var(X) = (E[x])^2 - (E[X])^2
+            if self.fill_nan is None:
+                cn = len(batch)
+                cv, cm = torch.var_mean(batch, axis=0, correction=0)
+            else:
+                cn = torch.sum(torch.isfinite(batch), axis=0)
+                cm = torch.nanmean(batch, axis=0)
+                cv = torch.nanmean(batch**2, axis=0) - (cm**2)
+            nan_mask = torch.isnan(cm)
+            # Merge the two sets of stats using the update from Knuth
+            N = pn + cn
+            cf = cn / N
+            pf = pn / N
+            dx = cm - pm
+            M = pm + (cf * dx)
+            V = (pv * pf) + (cv * cf) + (pf * cf * dx * dx)
+            # If any features were all-nan in this batch, the nans will still
+            # have propagated through nanmean: ignore those updates. N does not
+            # need updating (cn will already be 0 if the batch is all nan).
+            M[nan_mask] = self.running_mean[nan_mask]
+            V[nan_mask] = self.running_var[nan_mask]
+            # Clip the variances to avoid division by very small numbers
+            V = torch.clip(V, min=self.eps)
+            # Copy the results back into the module buffers
+            self.running_num.copy_(N)
+            self.running_var.copy_(V)
+            self.running_mean.copy_(M)
+
+    def apply_stats(self, batch):
+        out = (batch - self.running_mean) / torch.sqrt(self.running_var)
+        if self.fill_nan is None:
+            return out
+        else:
+            return torch.nan_to_num(out, nan=self.fill_nan)
+
+    def forward(self, batch):
+        # Use an additional flag to determine if should update the running
+        # stats. Then one is controlled by train/eval and one explicitly the
+        # user. So this layer can be pretrained, or trained only on the first
+        # epoch, to learn the mean/variance; then set `scaler.frozen = True`
+        # and the module will stop learning mean/variance independently of
+        # whether the model it is included in is training or evaluating.
+        if self.training and (not self.frozen):
+            self.update_running_stats(batch)
+        return self.apply_stats(batch)
---
+++ tests/neural_net/test_online_norm.py
@@ -0,0 +1,148 @@
+"""Test the online norm algorithm applied as a Pytorch module"""
+import unittest
+from copy import deepcopy
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from sklearn.preprocessing import StandardScaler
+
+from cml_tools.neural_net.online_norm import OnlineStandardScaler
+
+
+class TestOnlineStandardScaler(unittest.TestCase):
+    def setUp(self):
+        self.n_inst = 1024
+        self.n_feat = 121
+        # Choose n_batch so that it divides n_inst
+        self.n_batch = 32
+        # Used in every test: a scaler and some data to scale
+        self.X = torch.rand(self.n_inst, self.n_feat)
+        self.scaler = self.make_scaler(self.n_feat)
+
+    def make_scaler(self, N, eps=1e-5, fill_nan=None):
+        """Create a scaler and test that it is initialized correctly"""
+        scaler = OnlineStandardScaler(N, eps=eps, fill_nan=fill_nan)
+        scaler.train()
+        self.assertTrue(torch.all(scaler.running_num == 0.0))
+        self.assertTrue(torch.all(scaler.running_mean == 0.0))
+        self.assertTrue(torch.all(scaler.running_var == 1.0))
+        return scaler
+
+    def get_var_mean(self, X, nansafe=False):
+        if nansafe:
+            m = torch.nanmean(X, axis=0)
+            v = torch.nanmean(X**2, axis=0) - m**2
+        else:
+            v, m = torch.var_mean(X, axis=0, correction=0)
+        return v, m
+
+    def assert_torch_close(self, X, Y, atol=1e-8):
+        self.assertTrue(torch.allclose(X, Y, atol=atol, equal_nan=True))
+
+    def assert_fitness(self, X, scaler, n, nansafe=False):
+        """Check that scaler has been fit correctly to X over n steps"""
+        v, m = self.get_var_mean(X, nansafe=nansafe)
+        self.assertTrue(torch.all(scaler.running_num == n))
+        self.assert_torch_close(v, scaler.running_var.clone())
+        self.assert_torch_close(m, scaler.running_mean.clone())
+        # If nansafe, then Y = scaler(X) mean/variance will be altered by the
+        # fill values. So before testing that the results are correctly
+        # shifted, set them back to nan
+        scaler.eval()
+        Y = scaler(X)
+        Y[torch.isnan(X)] = torch.nan
+        yv, ym = self.get_var_mean(Y, nansafe=nansafe)
+        self.assert_torch_close(yv, torch.tensor(1.0))
+        # There are precision issues here - None of the numpy/torch et el
+        # methods of producing the mean and subtracting it from the vector
+        # produces a vector with zero-means... maybe this is fperr? I don't
+        # know quite what the correct value of atol is that checks *us* instead
+        # of checking torch. Perhaps need a method of quantifying the error.
+        self.assert_torch_close(ym, torch.tensor(0.0), atol=1e-5)
+
+    def assert_equality(self, s0, s1):
+        """Check that two scalers are identical"""
+        self.assertTrue(torch.all(s0.running_num == s1.running_num))
+        self.assertTrue(torch.all(s0.running_mean == s1.running_mean))
+        self.assertTrue(torch.all(s0.running_mean == s1.running_mean))
+
+    def test_batch_size_one(self):
+        # XXX: This is the test that requires atol=1e-5... the rest seem to
+        # work with atol=1e-7 or better. Accumulating the means one instance at
+        # a time perhaps introduces more error.
+        for x in self.X:
+            self.scaler(x.reshape(1, -1))
+        self.assert_fitness(self.X, self.scaler, self.n_inst)
+
+    def test_full_batch_size(self):
+        for batch in self.X.reshape(self.n_batch, -1, self.n_feat):
+            self.scaler(batch)
+        self.assert_fitness(self.X, self.scaler, self.n_inst)
+
+    def test_two_scalers_equal(self):
+        """Two OnlineStandardScalers should learn the same from the same data"""
+        scaler0 = self.make_scaler(self.n_feat)
+        scaler1 = self.make_scaler(self.n_feat)
+        for batch in self.X.reshape(self.n_batch, -1, self.n_feat):
+            x0 = scaler0(batch)
+            x1 = scaler1(batch)
+            self.assertTrue(torch.all(x0 == x1))
+        # They should still be correctly fitted as well as identical
+        self.assert_fitness(self.X, scaler0, self.n_inst)
+        self.assert_fitness(self.X, scaler1, self.n_inst)
+        self.assert_equality(scaler0, scaler1)
+
+    def test_scaler_stops_training_in_eval(self):
+        for batch in self.X.reshape(self.n_batch, -1, self.n_feat):
+            self.scaler(batch)
+        self.assert_fitness(self.X, self.scaler, self.n_inst)
+        frozen_scaler = deepcopy(self.scaler)
+        frozen_scaler.eval()
+        self.scaler.eval()
+        Y = torch.rand(self.n_inst, self.n_feat)
+        for batch in Y.reshape(self.n_batch, -1, self.n_feat):
+            y_scaled = self.scaler(batch)
+        self.assert_equality(self.scaler, frozen_scaler)
+
+    def test_scaler_stops_training_if_frozen(self):
+        """Test that a frozen scaler won't train even if torch sets #train()"""
+        for batch in self.X.reshape(self.n_batch, -1, self.n_feat):
+            self.scaler(batch)
+        self.assert_fitness(self.X, self.scaler, self.n_inst)
+        # Set the copy to be neither frozen nor training
+        frozen_scaler = deepcopy(self.scaler)
+        frozen_scaler.train(False)
+        self.assertFalse(frozen_scaler.frozen)
+        self.assertFalse(frozen_scaler.training)
+        # Set the scaler to be both frozen and training
+        self.scaler.frozen = True
+        self.scaler.train(True)
+        self.assertTrue(self.scaler.training)
+        self.assertTrue(self.scaler.frozen)
+        Y = torch.rand(self.n_inst, self.n_feat)
+        for batch in Y.reshape(self.n_batch, -1, self.n_feat):
+            self.scaler(batch)
+        self.assert_equality(self.scaler, frozen_scaler)
+
+    def test_scaler_with_nan_inputs(self):
+        fill_nan = 0.0
+        scaler = self.make_scaler(self.n_feat, fill_nan=fill_nan)
+        # Set the top right triangle of the input to NaN
+        X = self.X.clone()
+        X[*torch.triu_indices(self.n_feat, self.n_feat)] = torch.nan
+        # calculate the expected number of NaN per channel
+        N = torch.full((self.n_feat,), self.n_inst)
+        N = N - torch.arange(1, self.n_feat+1)
+        for batch in X.reshape(self.n_batch, -1, self.n_feat):
+            scaler(batch)
+        self.assert_fitness(X, scaler, N, nansafe=True)
+        # Check that Y are all finite and filled with fill_nan
+        scaler.eval()
+        Y = scaler(X)
+        self.assertTrue(torch.all(torch.isfinite(Y)))
+        self.assertTrue(torch.all(Y[torch.isnan(X)] == fill_nan))
+
+
+if __name__ == '__main__':
+    unittest.main()
